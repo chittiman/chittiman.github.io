c3b0ca3ea3c55bce4e45f60c109d49e5fa6e30d3
chittiman <chitreddysairam@gmail.com>
2024-03-07 20:35:02 +0530

    Cleaning conversion script

--- nb_to_md.py
+++ nb_to_md.py
@@ -4,26 +4,34 @@ import argparse
 import re
 
 cur_dir = Path.cwd()
-nb_dir = cur_dir / '_notebooks' # notebooks dir
-md_dir = cur_dir / '_posts'
-imgs_dir = cur_dir / 'assets' / 'image'
+nb_dir = cur_dir / '_notebooks' # notebooks folder
+md_dir = cur_dir / '_posts' # posts folder
+imgs_dir = cur_dir / 'assets' / 'image' #images folder
 
 def convert_nb_md(args):
+    #Notebook conversion to markdown
+    #Creates .md file and folder with assets used in notebook
     cmd = f"jupyter nbconvert --to markdown {args.file}"
     subprocess.run(cmd, shell=True)
     notebook = Path(args.file)
     
     file_prefix = notebook.name.replace('.ipynb', '')
 
+    #Moving images folder to right place
     img_dir = nb_dir / f'{file_prefix}_files'
     new_img_dir = imgs_dir / img_dir.name
-    img_dir.rename(new_img_dir)
+    if img_dir.exists():
+        img_dir.rename(new_img_dir)
 
     md_file = nb_dir / f'{file_prefix}.md'
-    clean_md_name = re.sub(r'[\s_]', '-',md_file.name).lower()#replacing space and _ with -
+    clean_md_name = re.sub(r'[\s_]', '-',md_file.name).lower()
+    #replacing space and _ with -
     new_md_name = f'{args.date}-{clean_md_name}'
     new_md_file = md_dir / new_md_name
+
+    #Cleaning markdown file
     clean_md(md_file,new_md_file,args)
+    #Deleting raw .md file
     md_file.unlink()
     #md_file.rename(new_md_file)
 
@@ -31,9 +39,9 @@ def convert_nb_md(args):
 def clean_md(src_file,tgt_file,args):
     text = load_file(src_file)
     text = re.sub(r'(!\[png\]\()(.*)\)',r'\1{{site.baseurl}}/assets/image/\2)' ,text)
-    #text = re.sub(r'(!\[png\]\()(.*)\)',r'<img src="/assets/image/\2"' ,text)
+    #Correcting assets folder path
 
-    #<img src="/assets/images/some_file_name.png">
+    # Adding front matter at the start
     front_matter = get_front_matter(args)
     clean_text = front_matter + "\n\n" + text
     write_file(tgt_file,clean_text)
@@ -46,14 +54,6 @@ def load_file(file):
 def write_file(file,text):
     with open(file, 'w+', encoding='utf-8') as f:
         f.write(text)
-
-# def clean_md_file(md_file):
-#     lines = load_file(md_file)
-    
-    
-
-
-
 
 def get_front_matter(args):
     cat_str = ', '.join(args.categories)
@@ -75,20 +75,6 @@ if __name__== '__main__':
     parser.add_argument('-title', type=str, help='title')
     parser.add_argument('-categories', nargs='*')
     args = parser.parse_args()
-#     text = load_file(args.file)
-
-# #![png](Introduction_to_Weight_Quantization_files/Introduction_to_Weight_Quantization_12_0.png)
-#     #matches = re.finditer(r'(!\[png\]\()(.*)\)', text)
-#     path = 'assets/image/'
-#     print(re.sub(r'(!\[png\]\()(.*)\)',r'\1assets/image/\2' ,text))
-    # print(type(matches[0]))
-    # for match in matches:
-    #     print(match.groups())
-    # # #print(get_front_matter(args))
-    #print(args.title)
-    #all_cats = ', '.join(args.categories)
-    #print(f'[{all_cats}]')
-
-    #'[%s]' % ', '.join(map(str, args.categories))
     convert_nb_md(args)
-    #print(load_file(args.file))
+    #Sample cmd 
+    #python ./nb_to_md.py -file ./_notebooks/test.ipynb -title "test post" -date "2024-01-02" -categories "testing" "experimenting"
